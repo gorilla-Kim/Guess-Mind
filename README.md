ed2b92ee2598ecc46f8ff83c5c79d1c57e372096
gorilla-Kim <00ghks22@naver.com>
2020-01-09 16:23:45 +0900

    views engine : pug

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Guess-Mind
+
+## MVC pattern
+
+## Model
+* dir :
+
+## View
+* dir : views
+* views engine : pug
+
+## Controller
+*  dir :
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 
 const PORT = 4000;
 const app = express();
+app.set("views engine", "pug");
 
 const handleListening = () => {
     console.log(`✅  Server is running! http://localhost:${ PORT }`);
