d490376eb7487a307a9b9e47054eb2a2f0e69680
gorilla-Kim <00ghks22@naver.com>
2020-01-13 17:41:35 +0900

    notification 일단 event에 대하여 socket에 연결하는 작업은 socket.js에서 다루고, 그에 따른 수행해야할 함수들은 각강의 js를 만들어서 관리를 한다.
    여기서는 notification.js를 만들어서 관련func을 만들어 socket.js 에서 import해 사용했다.
    
    handleNewUser 와 handleDisconnected를 만들어서 각각 유저의 유입과 연결종료에 따른 알림을 모든 유저들에게 전달하였다.

--- src/socketController.js
+++ src/socketController.js
@@ -3,10 +3,15 @@ import events from "./events";
 /* socket 관련 event handling을 해주는 controller 파일 */
 
 const socketController = socket => {
+  const broadcast = (event, data) => socket.broadcast.emit(event, data);
   // nickname 설정
   socket.on(events.setNickname, ({ nickname }) => {
     socket.nickname = nickname;
-    socket.broadcast.emit(events.newUser, { nickname });
+    broadcast(events.newUser, { nickname });
+  });
+  // 퇴장시... disconnect & disconnected
+  socket.on(events.disconnect, () => {
+    broadcast(events.disconnected, { nickname: socket.nickname });
   });
 };
 
